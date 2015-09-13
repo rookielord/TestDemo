ba04f81979a34100bc39dbb0baccf90a0f3aa9b2
juiz <510420060@qq.com>
2015-09-13 21:20:35 +0800

    滑动效果

---
+++ switchview/src/main/java/com/zhd/switchview/NewsFragment.java
@@ -0,0 +1,29 @@
+package com.zhd.switchview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by juiz on 2015/9/13.
+ */
+public class NewsFragment extends Fragment {
+    private String mWeburl;
+    private String mChannelName;
+    private View mView;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
